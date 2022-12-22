2e672fef5de4668faccf46fb3f67f8f6df150817
«Artur <y.artur2008@gmail.com>
2022-12-22 22:34:24 +0300

    вторая задача семинара

---
+++ lessons/Practik2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a % b == 0){
+    Console.WriteLine("кратно");
+}
+else{
+    Console.WriteLine("не кратно, остаток " + (a % b));
+}
