6066e0ba73d4b380d5e0871b154e3e0512c33be8
«Artur <y.artur2008@gmail.com>
2022-12-22 22:54:54 +0300

    четвертая задача семинара

---
+++ lessons/Practik2_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a * a == b || b * b == a){
+    Console.WriteLine("да");
+}
+else Console.WriteLine("нет");
