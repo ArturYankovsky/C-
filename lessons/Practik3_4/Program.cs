3b97320cf2cce5c8ad0eb01a790468c86f7a8948
«Artur <y.artur2008@gmail.com>
2022-12-24 18:58:27 +0300

    Пример 4 третьего семинара

---
+++ lessons/Practik3_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+
+for (int i = 1; i <= N; i++){
+    a = i * i;
+    Console.Write(a + " ");
+}
+
