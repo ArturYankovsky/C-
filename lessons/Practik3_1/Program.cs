f5700d27a35ae386bdb579c38493d83ec5a97d51
«Artur <y.artur2008@gmail.com>
2022-12-24 18:10:03 +0300

    Пример 1 третьего семинара

---
+++ lessons/Practik3_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите координату x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+if (x > 0 && y > 0) Console.WriteLine("I");
+else if (x < 0 && y > 0) Console.WriteLine("II");
+else if (x < 0 && y < 0) Console.WriteLine("III");
+else Console.WriteLine("IV");
