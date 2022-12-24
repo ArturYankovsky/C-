de92285f616050472f48e4f12514e7908fdf5a1e
«Artur <y.artur2008@gmail.com>
2022-12-24 18:22:42 +0300

    Пример 2 третьего семинара

---
+++ lessons/Practik3_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите номер четверти: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x < 1 | x > 4) Console.WriteLine("Неправильный номер четверти, их всего 4!");
+else if (x == 1) Console.WriteLine("(x>0,y>0)");
+else if (x == 2) Console.WriteLine("(x<0,y>0)");
+else if (x == 3) Console.WriteLine("(x<0,y<0)");
+else if (x == 4) Console.WriteLine("(x>0,y<0)");
