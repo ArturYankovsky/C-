71d98b652611e288e7e44d6c060cc1c683088a2a
«Artur <y.artur2008@gmail.com>
2022-12-24 17:02:38 +0300

    Домашняя задача 13 семинара

---
+++ lessons/Practik2_dz_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = a;
+int i1 = 0;
+
+if (a / 100 == 0) Console.WriteLine("третьей цифры нет");
+else {
+    while (i / 1000 != 0){
+        i = i / 10;
+    }
+i1 = i % 10;
+Console.WriteLine(i1);}
