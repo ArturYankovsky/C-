20ac31c9e62a0ba8a059772b22fbb85aa614bef4
«Artur <y.artur2008@gmail.com>
2022-12-25 13:45:26 +0300

    Домашняя задача 19 семинара

---
+++ lessons/Practik3_dz_1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+//Console.WriteLine("Введите пятизначное число: ");
+//string n = Console.ReadLine();
+
+//if (n.Length == 5){
+    //if (n[1] == n[5] && n[2] == n[4]) Console.WriteLine("Да");
+   // else Console.WriteLine("Нет");
+
+//else Console.WriteLine("Вы ввели не пятизначное число!");
+
+Console.WriteLine("Введите пятизначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a / 100000 == 0 && a / 10000 != 0){
+    int a1 = a / 10000;
+    int a2 = a % 10;
+    int a3 = a / 1000;
+    int a4 = a3 % 10;
+    int a5 = a % 100;
+    int a6 = a5 / 10;
+    if (a1 == a2 && a4 == a6) Console.WriteLine ("Да");
+    else Console.WriteLine("Нет");
+}
+else Console.WriteLine ("Вы ввели не пятизначное число!");
