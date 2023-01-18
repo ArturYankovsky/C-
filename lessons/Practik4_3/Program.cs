52ede0fee1d40bf014575fb20c443c2937024647
«Artur <y.artur2008@gmail.com>
2023-01-18 23:29:30 +0300

    Функции

---
+++ lessons/Practik4_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// function
+// Вывести наибольшее число из двух
+
+int test(int a, int b)
+{
+    if(a > b)
+        return a;
+    return b;
+}
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(test(n , m));
+
+
+
