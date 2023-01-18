88fbfca1dd9ddcdae39f1b98facd85206028ff74
«Artur <y.artur2008@gmail.com>
2023-01-19 00:42:18 +0300

    задача 30

---
+++ lessons/Practik4_8/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу, которая заполняет массив из 8ми элементов нулями и единицами в случайном порядке.
+
+Console.Clear();
+int n = 8;
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0,2);
+Console.WriteLine($"[{string.Join(", ", array)}]");
