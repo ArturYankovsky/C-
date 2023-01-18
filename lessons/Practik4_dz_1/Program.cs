12b7cc7c6cd8e3b2d9ddb5f7b3c87b9c3abdbe66
«Artur <y.artur2008@gmail.com>
2023-01-19 01:17:04 +0300

    домашняя задача 25

---
+++ lessons/Practik4_dz_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите два числа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+int proizv = 1;
+for(int i = 1; i <= m; i++)
+{
+    proizv = proizv * n;
+}
+
+Console.Write(proizv);
