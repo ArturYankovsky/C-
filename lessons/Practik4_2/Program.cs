c1e0b6d2925fb4d6e6e480c7c4e5643d49309020
«Artur <y.artur2008@gmail.com>
2023-01-18 23:10:13 +0300

    Массив double

---
+++ lessons/Practik4_2/Program.cs
@@ -0,0 +1,8 @@
+﻿// array double
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = Math.Round(new Random().NextDouble() * (50 - 10) + 10, 2);
+Console.WriteLine($"[{string.Join(" ; ", array)}]");
