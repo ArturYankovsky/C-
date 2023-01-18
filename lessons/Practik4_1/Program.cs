de1a84961faed483d807e1162f0198581808a042
«Artur <y.artur2008@gmail.com>
2023-01-18 23:00:54 +0300

    Массив

---
+++ lessons/Practik4_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// array
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1,11);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+
+
