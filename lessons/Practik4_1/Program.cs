03379544c1c136c83cdf2dcf6fb730631a3b569b
«Artur <y.artur2008@gmail.com>
2023-01-19 23:10:59 +0300

    домашняя задача 29

--- lessons/Practik4_1/Program.cs
+++ lessons/Practik4_1/Program.cs
@@ -5,6 +5,7 @@ int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 for (int i = 0; i < array.Length; i++)
     array[i] = new Random().Next(1,11);
+    
 Console.WriteLine($"[{string.Join(", ", array)}]");
 
 
---
+++ lessons/Practik4_dz_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int[] array = new int[8];
+
+Console.Write("Введите элементы массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 7;
+
+while(n > 0)
+    {
+        int a = n % 10;
+        array[i] = a;
+        n = n / 10;
+        i--;
+    }
+    
+Console.WriteLine($"[{string.Join(", ", array)}]");
