9434763f5f094e5c65887c0e6cd2c35bab6ee4b2
«Artur <y.artur2008@gmail.com>
2023-01-19 22:24:00 +0300

    домашняя задача 27

---
+++ lessons/Practik4_dz_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (n > 0)
+{
+    int a = n % 10;
+    n /= 10;
+    sum = sum + a;
+}
+Console.Write(sum);
