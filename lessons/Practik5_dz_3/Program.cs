89bdc0b31063c6654e9703c33d5f888442e2c092
«Artur <y.artur2008@gmail.com>
2023-01-22 15:10:57 +0300

    домашняя задача 38

---
+++ lessons/Practik5_dz_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 38: Задайте массив чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+
+void input_array(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 11);
+}
+
+void Razn(int[] array)
+{
+    int max = 0; int min = 0; int razn = 0;
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[max]) max = j;
+            else
+                if(array[j] < array[min]) min = j;
+        }
+    razn = array[max] - array[min];
+    Console.WriteLine(razn);
+    return;
+    }
+    
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+input_array(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Razn(array);
