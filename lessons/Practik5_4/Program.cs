319238463a9d5f074cad015f80171ca9db8bc572
«Artur <y.artur2008@gmail.com>
2023-01-22 03:00:08 +0300

    практика задача 35

---
+++ lessons/Practik5_4/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
+//значения которых лежат в отрезке [10,99].
+//Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+//[5, 18, 123, 6, 2] -> 1
+//[1, 2, 3, 6, 2] -> 0
+//[10, 11, 12, 13, 14] -> 5
+
+
+
+void input_array(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 101); //[-100, 100]
+}
+
+int poisk(int[] array)
+{
+    int m = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] >= 10 && array[i] <= 99)
+            m++;
+    return m;
+Console.WriteLine(m);
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+input_array(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(poisk(array));
