e105d7d4d4048493e56b3a122ed84a0ca7d830ee
«Artur <y.artur2008@gmail.com>
2023-01-25 21:45:56 +0300

    решение Рустемом домашней задачи

---
+++ lessons/Practik6_2/Program.cs
@@ -0,0 +1,19 @@
+﻿double test(double[] array)
+{
+double min = array[0];
+double max = array[0];
+for (int i = 0; i < array.Length; i ++){
+if (min > array[i])
+min = array[i];
+if (array[i] > max)
+max = array[i];
+}
+
+foreach (double i in array){
+if (min > i)
+min = i;
+if (i > max)
+max = i;
+}
+return max - min;
+}
