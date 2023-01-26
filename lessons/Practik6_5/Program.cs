fd59cfd4d5d0ee35155e191dd59537e54708192d
«Artur <y.artur2008@gmail.com>
2023-01-26 20:19:07 +0300

    практика задача 42 преобразование в двоичное число

---
+++ lessons/Practik6_5/Program.cs
@@ -0,0 +1,14 @@
+﻿// **Задача 42:** Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+string res = "";
+while(n > 0)
+{
+res = Convert.ToString(n % 2) + res;
+n /= 2;
+}
+Console.WriteLine(res);
