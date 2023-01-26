8ad0ffc2592d1c81c2f5030cfc1d9894a0614b31
«Artur <y.artur2008@gmail.com>
2023-01-26 22:33:53 +0300

    домашняя задача 41

---
+++ lessons/Practik6_dz_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.WriteLine("Введите числа через пробел: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int m = 0;
+
+for(int i = 0; i < size.Length; i++)
+{
+    if(size[i] > 0) m = m + 1;
+}
+
+Console.WriteLine($"{"Количество чисел больше нуля: "}{m}");
