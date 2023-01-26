fad0952ab3926143f4241a72a353fbcecc6d4da4
«Artur <y.artur2008@gmail.com>
2023-01-26 21:02:34 +0300

    практика задача 45 поэлементное копирование

---
+++ lessons/Practik6_7/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+Console.Clear();
+int[] a = {1, 2, 3};
+int[] b = new int[a.Length];
+for (int i = 0; i < a.Length; i++)
+b[i] = a[i];
+Console.WriteLine(string.Join(" ", a));
+Console.WriteLine(string.Join(" ", b));
