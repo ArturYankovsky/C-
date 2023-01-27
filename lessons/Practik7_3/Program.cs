a5324f0dbae2afbfd0b842632fdf95c0183d3aac
«Artur <y.artur2008@gmail.com>
2023-01-27 23:27:39 +0300

    практика задача 49

---
+++ lessons/Practik7_3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11); // [-10;10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+void Release(int[,] matrix){
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (i % 2 == 1 && j % 2 == 1)
+        matrix[i,j] *= matrix[i,j];
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Начальная матрица: ");
+InputMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine("Итоговая матрица: ");
+Release(matrix);
