a76dcb9cea96b5a9e7aef4a7aedd8fc7138d21f4
«Artur <y.artur2008@gmail.com>
2023-02-05 16:31:38 +0300

    практика РЕКУРСИЯ вывод от N до M

--- lessons/Practik9_3/Program.cs
+++ lessons/Practik9_3/Program.cs
@@ -1,4 +1,7 @@
-﻿
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
 
 string rec(int m, int n)
 {
@@ -10,6 +13,6 @@ string rec(int m, int n)
  Console.Clear();
  Console.Write("Введите первое число: ");
  int n = Convert.ToInt32(Console.ReadLine());
- Console.Write("Введите первое число: ");
+ Console.Write("Введите второе число: ");
  int m = Convert.ToInt32(Console.ReadLine());
  Console.WriteLine(rec(n, m));
