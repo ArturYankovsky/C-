36f67280b355a72731eec915cdf4baf64885cd85
«Artur <y.artur2008@gmail.com>
2023-02-05 16:16:01 +0300

    практика РЕКУРСИЯ вывод от 1 до 5

--- lessons/Practik9_2/Program.cs
+++ lessons/Practik9_2/Program.cs
@@ -1,15 +1,20 @@
-﻿
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
 
-string rec(int m, int n)
+
+string rec(int n)
 {
-     if (n == m)
-         return $"{n}";
-     return rec(m, n - 1) + $" {n}";
- }
+    if(n == 1)
+        return "1";
+    return rec(n - 1) + $" {n}";
+}
 
- Console.Clear();
- Console.Write("Введите первое число: ");
- int n = Convert.ToInt32(Console.ReadLine());
- Console.Write("Введите первое число: ");
- int m = Convert.ToInt32(Console.ReadLine());
- Console.WriteLine(rec(n, m));
+Console.Clear();
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(rec(n));
