f304aac93420e787448c4264c8350d8bee305e73
«Artur <y.artur2008@gmail.com>
2023-02-05 16:47:26 +0300

    практика РЕКУРСИЯ а в степени b

---
+++ lessons/Practik9_5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int rec(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    return rec(a, b - 1) * a;
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(a, b));
